8c72a65c7175c042a99e47d642458094720ec078
manoj <dearsatya19@gmail.com>
2024-02-18 17:30:11 +0000

    Your commit message here

---
+++ uma.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Define the source repositories and target repository
+
+# List of repositories to clone
+source_repos=(
+    "git@github.com:manoj5606/maven-web-application.git"
+    "git@github.com:manoj5606/Facebook-for-manoj.git"
+
+)
+
+#target repository
+
+target_repo="git@github.com:satya19-wipro/kashu-test.git"
+
+#Loop through each sorce repository
+
+for repo in "${source_repos[@]}"; do
+    #clone the source repository
+    #mkdir "$source_repos[@]"
+    git clone "$repo" #${source_repos[@]}
+    pwd
+    git remote -v
+    ls
+    #change directory to the cloned repository
+    cd "$repo"
+    #cd ${source_repos[@]} || exit
+
+    git pull origin master
+    ls
+    #Add the target repository as a remote
+    
+    git remote add target-url "$target_repo"
+
+    git fetch target-url master
+    #git pull --ff-only
+    
+    git merge target-url/master --allow-unrelated-histories
+    #push changes to the target repository
+    
+    git push target-url master
+
+    #navigate back to the original directory
+    #rm -rf temp_repo
+    cd ..
+    #git remote target-url master
+
+    echo "current dir ls"
+    ls
+    #rm -rf temp_repo
+done
+
+# Clean up temporary directory
+
+#rm -rf $temp_repo
+
+#echo "All repositories copied to target repository."
